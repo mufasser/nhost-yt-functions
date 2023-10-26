bef1baf9893e187a989cf52cd60220ae1cdfe2e2
Mufasser <mufasseri@gmail.com>
2023-10-26 17:42:45 +0500

    api updated

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,9 +1,9 @@
-import { uploadToBucket } from '../_utils/uploadBucket.js'
+// import { uploadToBucket } from '../_utils/uploadBucket.js'
 
 export default async (req, res) => {
-  let uploadedFileUrl = await uploadToBucket();
-    // res.status(200).send(`Uploaded Url ${req.query.name}!`)
-    res.status(200).send(`Uploaded Url ${uploadedFileUrl}!`)
+  // let uploadedFileUrl = await uploadToBucket();
+    res.status(200).send(`Uploaded Url ${req.query.name}!`)
+    // res.status(200).send(`Uploaded Url ${uploadedFileUrl}!`)
   }
   
   
