24d393989a3a8fb8279e89bdb487008fcdae426d
Mufasser <mufasseri@gmail.com>
2023-10-26 17:31:21 +0500

    storage code added

---
+++ functions/_utils/client.js
@@ -0,0 +1,18 @@
+import { NhostClient } from '@nhost/nhost-js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+/**
+ * Create a new Nhost client.
+ *
+ * @returns {NhostClient} A new Nhost client.
+ */
+// TODO;
+export function createClient() {
+  return new NhostClient({
+    subdomain: 'yfnaaqdhstomxncwctej',
+    region: 'ap-south-1',
+    adminSecret: 'o_A^0hsy0w!yv+Wb_q_ggrz__I8dWs5i'
+  })
+}  
--- functions/api/index.js
+++ functions/api/index.js
@@ -1,5 +1,9 @@
-export default (req, res) => {
-    res.status(200).send(`Hello ${req.query.name}!`)
+import { uploadToBucket } from '../_utils/uploadBucket.js'
+
+export default async (req, res) => {
+  let uploadedFileUrl = await uploadToBucket();
+    // res.status(200).send(`Uploaded Url ${req.query.name}!`)
+    res.status(200).send(`Uploaded Url ${uploadedFileUrl}!`)
   }
   
   
