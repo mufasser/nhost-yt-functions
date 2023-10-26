5668f43e2691d4aee18d7e42e512c383d0c0f9a8
Mufasser <mufasseri@gmail.com>
2023-10-26 17:45:31 +0500

    new code commented

--- functions/_utils/uploadBucket.js
+++ functions/_utils/uploadBucket.js
@@ -1,69 +1,69 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-import { createClient } from './client.js'
+// import fs from 'fs'
+// import fetch from 'node-fetch'
+// import { createClient } from './client.js'
 
-const client = createClient()
+// const client = createClient()
 
-export async function uploadToBucket() {
-  console.info('Uploading a Single File to a custom bucket...')
+// export async function uploadToBucket() {
+//   console.info('Uploading a Single File to a custom bucket...')
 
-  try {
-    // Download image from remote URL
-    const response = await fetch(
-      'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=1.00xw:0.753xh;0,0.153xh&resize=1200:*'
-    )
+//   try {
+//     // Download image from remote URL
+//     const response = await fetch(
+//       'https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=1.00xw:0.753xh;0,0.153xh&resize=1200:*'
+//     )
 
-    if (!response.ok) {
-      console.error(`[file-to-bucket]`, 'Image not found!')
+//     if (!response.ok) {
+//       console.error(`[file-to-bucket]`, 'Image not found!')
 
-      return
-    }
+//       return
+//     }
 
-    const arrayBuffer = await response.arrayBuffer()
+//     const arrayBuffer = await response.arrayBuffer()
 
-    const fileBuffer = Buffer.from(arrayBuffer)
-    const fileName = 'cat.jpg'
+//     const fileBuffer = Buffer.from(arrayBuffer)
+//     const fileName = 'cat.jpg'
 
-    fs.writeFile(fileName, fileBuffer, async (err) => {
-      if (err) {
-        console.error(`[file-to-bucket]`, err)
-        return
-      }
+//     fs.writeFile(fileName, fileBuffer, async (err) => {
+//       if (err) {
+//         console.error(`[file-to-bucket]`, err)
+//         return
+//       }
 
-      const file = fs.createReadStream(fileName)
+//       const file = fs.createReadStream(fileName)
 
-      const { error: uploadError, fileMetadata } = await client.storage.upload({
-        file,
-        bucketId: 'custom'
-      })
+//       const { error: uploadError, fileMetadata } = await client.storage.upload({
+//         file,
+//         bucketId: 'custom'
+//       })
 
-      if (uploadError) {
-        console.error(`[file-to-bucket]`, uploadError)
+//       if (uploadError) {
+//         console.error(`[file-to-bucket]`, uploadError)
 
-        return
-      }
+//         return
+//       }
 
-      console.info(`[file-to-bucket]`, `File has been uploaded successfully!`)
-      console.info(`[file-to-bucket]`, `ID: ${fileMetadata?.id}`)
+//       console.info(`[file-to-bucket]`, `File has been uploaded successfully!`)
+//       console.info(`[file-to-bucket]`, `ID: ${fileMetadata?.id}`)
 
-      console.log(fileMetadata.bucketId)
+//       console.log(fileMetadata.bucketId)
 
-      // Generate a presigned URL for the uploaded file
-      const { error: presignError, presignedUrl: image } = await client.storage.getPresignedUrl({
-        fileId: fileMetadata.id
-      })
+//       // Generate a presigned URL for the uploaded file
+//       const { error: presignError, presignedUrl: image } = await client.storage.getPresignedUrl({
+//         fileId: fileMetadata.id
+//       })
 
-      if (presignError) {
-        console.error(`[file-to-bucket]`, presignError)
-        return
-      }
+//       if (presignError) {
+//         console.error(`[file-to-bucket]`, presignError)
+//         return
+//       }
 
-      console.info(`[file-to-bucket]`, `Presigned URL: ${image.url}`)
-      return image.url;
-    })
+//       console.info(`[file-to-bucket]`, `Presigned URL: ${image.url}`)
+//       return image.url;
+//     })
 
-    // Upload file to Nhost Storage
-  } catch (error) {
-    console.error(`[file-to-bucket]`, error.message)
-  }
-}
+//     // Upload file to Nhost Storage
+//   } catch (error) {
+//     console.error(`[file-to-bucket]`, error.message)
+//   }
+// }
