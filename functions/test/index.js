dbcfa336dcc2859a7e80b41f47b3ffca5efa50f1
Mufasser <mufasseri@gmail.com>
2023-10-26 18:08:37 +0500

    new test endpoint created

---
+++ functions/test/index.js
@@ -0,0 +1,6 @@
+export default async (req, res) => {
+    // let uploadedFileUrl = await uploadToBucket();
+      res.status(200).send(`Uploaded Url ${req.query.name}!`)
+      // res.status(200).send(`Uploaded Url ${uploadedFileUrl}!`)
+    }
+    
